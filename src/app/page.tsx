8503a95eaf29954665aadece6799dd8082c940b8
Evan <marcel.evan2001@gmail.com>
2024-03-28 09:58:49 +0100

    setup mapbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,48 @@
-import Image from "next/image";
+'use client';
+import Map, { Marker, GeolocateControl, NavigationControl } from "react-map-gl";
+import { useState } from "react";
+import "mapbox-gl/dist/mapbox-gl.css";
+import classes from "./Page.module.css";
 
 export default function Home() {
+
+  interface Marker {
+    longitude: number;
+    latitude: number;
+  }
+
+  const [markers, setMarkers] = useState<Marker[]>([]);
+
+  const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_APIKEY;
+
+  const handleMapClick = (event: { lngLat: any; }) => {
+    const { lngLat } = event;
+    const newMarker = {
+      longitude: lngLat.lng,
+      latitude: lngLat.lat
+    };
+    setMarkers([...markers, newMarker]);
+  }
+
   return (
-    <h1>
-      G&apos;le Point
-    </h1>
+    <main className={classes.mainStyle}>
+      <Map mapboxAccessToken={mapboxToken}
+        mapStyle="mapbox://styles/mapbox/streets-v12"
+        style={{ width: "100%", height: "100%" }}
+        initialViewState={{ latitude: 35.668641, longitude: 139.750567, zoom: 10 }}
+        maxZoom={30}
+        minZoom={3}
+        onClick={handleMapClick}
+      >
+        <GeolocateControl position="top-left" />
+        <NavigationControl position="top-left" />
+        {markers.map((marker, index) => (
+          <Marker key={index} longitude={marker.longitude} latitude={marker.latitude}/>
+        ))}
+      </Map>
+
+      <button onClick={() => setMarkers([])}>Clear</button>
+
+    </main>
   );
 }

9991aa594e7e5dbfd3739b2c88c312f0cf45e4e2
Evan EMA. MARCEL <emarcel@asylum.local>
2024-04-04 14:04:19 +0200

    add markers

--- src/app/db/firebase.ts
+++ src/app/db/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,10 +15,12 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+const db = getFirestore();
+export { auth, db };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-'use client';
-import Map, { Marker, GeolocateControl, NavigationControl } from "react-map-gl";
-import { useState } from "react";
-import "mapbox-gl/dist/mapbox-gl.css";
-import classes from "./Page.module.css";
+"use client";
+import { db } from "@/app/db/firebase";
+import { useAuthStore } from "@/app/store/authStore";
 import { Button } from "@/components/ui/button";
+import { addDoc, collection } from "firebase/firestore";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useState } from "react";
+import Map, { GeolocateControl, Marker, NavigationControl } from "react-map-gl";
+import classes from "./Page.module.css";
 
 export default function Home() {
+  const { user } = useAuthStore();
 
   interface Marker {
     longitude: number;
@@ -16,25 +20,35 @@ export default function Home() {
 
   const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_APIKEY;
 
-  const handleMapClick = (event: { lngLat: any; }) => {
+  const handleMapClick = (event: { lngLat: any }) => {
     const { lngLat } = event;
     const newMarker = {
       longitude: lngLat.lng,
-      latitude: lngLat.lat
+      latitude: lngLat.lat,
     };
     setMarkers([...markers, newMarker]);
-  }
+    const colref = collection(db, "markers");
+    addDoc(colref, {
+      location: newMarker.latitude + "," + newMarker.longitude,
+      user: user?.uid,
+    });
+  };
 
   const clearMarker = () => {
     setMarkers([]);
-  }
+  };
 
   return (
     <main className={classes.mainStyle}>
-      <Map mapboxAccessToken={mapboxToken}
+      <Map
+        mapboxAccessToken={mapboxToken}
         mapStyle="mapbox://styles/mapbox/streets-v12"
         style={{ width: "100%", height: "100%" }}
-        initialViewState={{ latitude: 35.668641, longitude: 139.750567, zoom: 10 }}
+        initialViewState={{
+          latitude: 35.668641,
+          longitude: 139.750567,
+          zoom: 10,
+        }}
         maxZoom={30}
         minZoom={3}
         onClick={handleMapClick}
@@ -42,14 +56,17 @@ export default function Home() {
         <GeolocateControl position="top-left" />
         <NavigationControl position="top-left" />
         {markers.map((marker, index) => (
-          <Marker key={index} longitude={marker.longitude} latitude={marker.latitude}/>
+          <Marker
+            key={index}
+            longitude={marker.longitude}
+            latitude={marker.latitude}
+          />
         ))}
       </Map>
 
       <Button variant="default" size="sm" onClick={clearMarker}>
         Clear Marker
       </Button>
-
     </main>
   );
 }

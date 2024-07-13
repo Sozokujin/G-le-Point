fcab9e9ac061a722a6d75ab2267db7971729c6e6
Léo Corre <lcorre.pro@gmail.com>
2024-07-13 22:25:11 +0200

    fix(map): src images & error console map page

--- src/app/(map)/layout.tsx
+++ src/app/(map)/layout.tsx
@@ -2,11 +2,9 @@ import Navbar from "@/components/navbar";
 
 export default function MapLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fr">
-      <body>
-        {children}
-        <Navbar />
-      </body>
-    </html>
+    <>
+      {children}
+      <Navbar />
+    </>
   );
 }

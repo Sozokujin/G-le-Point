4c67221768c66c16950954598075941fbb76bf0c
Léo Corre <lcorre.pro@gmail.com>
2024-08-15 23:59:31 +0200

    fix(home): remove padding top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,7 +113,7 @@ export default function Page() {
               }}
             />
           </div>
-          <div id="presentation" className="py-24 sm:py-32">
+          <div id="presentation" className="pt-24 sm:pt-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
               <div className="mx-auto max-w-2xl text-center">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">

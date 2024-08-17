3f22cab18552a920393ff7e13b8cafced8926601
evan <marcel.evan2001@gmail.com>
2024-08-17 21:08:05 +0200

    add(friends & group):popup component

--- src/components/friends/modalSendFriendRequest.tsx
+++ src/components/friends/modalSendFriendRequest.tsx
@@ -6,8 +6,10 @@ import {
 } from "@/components/ui/dialog";
 import { sendFriendRequest } from "@/services/firebase/friends";
 import { useRef, useState } from "react";
+import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { Toaster } from "../ui/sonner";
 
 export const ModalSendFriendRequest = () => {
   const [showPopupCopy, setShowPopupCopy] = useState(false);
@@ -19,35 +21,35 @@ export const ModalSendFriendRequest = () => {
     invitationCode: string | undefined
   ) => {
     await sendFriendRequest(invitationCode);
-    setShowPopupSend(true);
-    setTimeout(() => {
-      setShowPopupSend(false);
-    }, 3000);
+    toast("Demande d'ami envoyée !");
   };
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button className="btn btn-primary">Ajouter un ami</Button>
-      </DialogTrigger>
-      <DialogContent>
-        <h2 className="text-primary text-xl font-bold">Ajouter un ami</h2>
-        <Input
-          ref={invitationCodeRef}
-          placeholder="Entrez le code d'invitation de votre ami"
-          id="invitationCode"
-          type="text"
-        />
-        <DialogClose asChild>
-          <Button
-            onClick={() =>
-              handleSendFriendRequest(invitationCodeRef.current?.value)
-            }
-          >
-            Envoyer la demande
-          </Button>
-        </DialogClose>
-      </DialogContent>
-    </Dialog>
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="btn btn-primary">Ajouter un ami</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <h2 className="text-primary text-xl font-bold">Ajouter un ami</h2>
+          <Input
+            ref={invitationCodeRef}
+            placeholder="Entrez le code d'invitation de votre ami"
+            id="invitationCode"
+            type="text"
+          />
+          <DialogClose asChild>
+            <Button
+              onClick={() =>
+                handleSendFriendRequest(invitationCodeRef.current?.value)
+              }
+            >
+              Envoyer la demande
+            </Button>
+          </DialogClose>
+        </DialogContent>
+      </Dialog>
+      <Toaster position="top-right" />
+    </>
   );
 };

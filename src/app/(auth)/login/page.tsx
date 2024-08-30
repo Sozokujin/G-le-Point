2d84f0122c0ad08bbe4fd0c448c632a4b87eea94
Kévin <kevin.blaise24@gmail.com>
2024-08-30 23:06:44 +0200

    feat(refacto): clean login page code using layout

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -24,15 +24,6 @@ const Login = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-  useEffect(() => {
-    if (errorMessage) {
-      const timer = setTimeout(() => {
-        setErrorMessage(null);
-      }, 5000);
-      return () => clearTimeout(timer);
-    }
-  }, [errorMessage]);
-
   const addToDbIfNewUser = async (user: any) => {
     const usersCollectionRef = collection(db, "users");
     const _query = query(usersCollectionRef, where("uid", "==", user.uid));
@@ -49,6 +40,7 @@ const Login = () => {
 
   const signInWithProvider = async (provider: any | null) => {
     if (!provider || isLoading) return;
+    setErrorMessage(null);
     setIsLoading(true);
 
     try {
@@ -62,10 +54,10 @@ const Login = () => {
           email: result.user.email,
           photoURL: result.user.photoURL,
         };
-        
+
         addToDbIfNewUser(firebaseUser);
         setUser(firebaseUser);
-        
+
         await fetch("/api/login", {
           headers: {
             Authorization: `Bearer ${idToken}`,
@@ -121,79 +113,53 @@ const Login = () => {
   ];
 
   return (
-    <div className="w-full h-[100svh] flex justify-center items-center lg:grid lg:grid-cols-2">
-      <div
-        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-glp-green-200 to-glp-green-600 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
+    <div className="mx-auto grid w-[350px] gap-6">
+      <div className="grid gap-2 text-center">
+        <Link href="/">
+          <Image
+            src={"/images/main-logo-green.png"}
+            alt="Logo G'Le Point"
+            width={128}
+            height={128}
+            className="mx-auto"
+          />
+        </Link>
+        <h1 className="text-3xl font-bold">
+          Commencez à explorer de nouveaux horizons
+        </h1>
+        <p className="text-balance text-muted-foreground">
+          Veuillez choisir votre méthode de connexion :
+        </p>
       </div>
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6">
-          <div className="grid gap-2 text-center">
-            <Link href="/">
-              <Image
-                src={"/images/main-logo-green.png"}
-                alt="Logo G'Le Point"
-                width={128}
-                height={128}
-                className="mx-auto"
-              />
-            </Link>
-            <h1 className="text-3xl font-bold">
-              Commencez à explorer de nouveaux horizons
-            </h1>
-            <p className="text-balance text-muted-foreground">
-              Veuillez choisir votre méthode de connexion :
-            </p>
+      <div className="grid gap-4 relative">
+        {isLoading && (
+          <div className="absolute h-full w-full flex items-center justify-center z-10">
+            <DotLoader color="#37b978" />
           </div>
-          <div className="grid gap-4 relative">
-            {isLoading && (
-              <div className="absolute h-full w-full flex items-center justify-center z-10">
-                <DotLoader color="#37b978" />
-              </div>
-            )}
-            {tierceApps.map((app) => (
-              <Button
-                key={app.name}
-                variant="outline"
-                className={`w-full flex justify-center items-center gap-4 ${
-                  app.active
-                    ? ""
-                    : "opacity-50 cursor-not-allowed text-gray-600 border-gray-600 hover:bg-gray-600 hover:text-white"
-                } ${isLoading ? "cursor-wait blur" : ""}`}
-                onClick={() => signInWithProvider(app.provider)}
-              >
-                <Image src={app.icon} alt="" width={16} height={16} />
-                Se connecter avec {app.name}
-              </Button>
-            ))}
+        )}
+        {tierceApps.map((app) => (
+          <Button
+            key={app.name}
+            variant="outline"
+            className={`w-full flex justify-center items-center gap-4 ${app.active
+                ? ""
+                : "opacity-50 cursor-not-allowed text-gray-600 border-gray-600 hover:bg-gray-600 hover:text-white"
+              } ${isLoading ? "cursor-wait blur" : ""}`}
+            onClick={() => signInWithProvider(app.provider)}
+          >
+            <Image src={app.icon} alt="" width={16} height={16} />
+            Se connecter avec {app.name}
+          </Button>
+        ))}
 
-            <Link href="/" className="text-center text-xs underline">
-              Retourner sur la page d'accueil
-            </Link>
-            {errorMessage && (
-              <div className="absolute top-4 right-4 h-auto w-48 bg-glp-green-600 p-5 rounded-sm flex justify-center items-center text-white">
-                {errorMessage}
-              </div>
-            )}
+        <Link href="/" className="text-center text-xs underline">
+          Retourner sur la page d'accueil
+        </Link>
+        {errorMessage && (
+          <div className="absolute top-4 right-4 h-auto w-48 bg-glp-green-600 p-5 rounded-sm flex justify-center items-center text-white">
+            {errorMessage}
           </div>
-        </div>
-      </div>
-      <div className="h-full hidden bg-muted lg:block">
-        <Image
-          src="/images/wallpaper.jpg"
-          alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+        )}
       </div>
     </div>
   );

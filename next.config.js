cb89f1962e1e6cf81f7e5f28a78f4f7cc20b2f79
Léo Corre <lcorre.pro@gmail.com>
2024-09-02 17:11:09 +0200

    feat(config): add twitter remotepatterns to see profile picture

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ module.exports = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };

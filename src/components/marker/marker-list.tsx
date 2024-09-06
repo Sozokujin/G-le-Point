15606aad55d6d925b5e57423b8f8157b806e422c
oltean-petru <poltean33@gmail.com>
2024-09-06 23:02:25 +0200

    feat(friends): styled marker list for desktop/mobile

--- src/components/marker/marker-list.tsx
+++ src/components/marker/marker-list.tsx
@@ -16,16 +16,15 @@ import {
 } from "@heroicons/react/24/solid";
 import { Button } from "../ui/button";
 import { useState } from "react";
+import useIsMobile from "@/utils/isMobile";
 
 interface MarkerListProps {
   markers: Marker[];
 }
 
 export const MarkerList: React.FC<MarkerListProps> = ({ markers }) => {
-  const [showPopupCopy, setShowPopupCopy] = useState<{
-    [key: string]: boolean;
-  }>({});
-
+  const [showPopupCopy, setShowPopupCopy] = useState<{[key: string]: boolean}>({});
+  const isMobile = useIsMobile();
   const copyToClipboard = (marker: Marker) => {
     navigator.clipboard.writeText(marker.latitude + " , " + marker.longitude);
     setShowPopupCopy((prev) => ({ ...prev, [marker.id]: true }));
@@ -41,49 +40,82 @@ export const MarkerList: React.FC<MarkerListProps> = ({ markers }) => {
           <p className="text-lg text-slate-400">Aucun Point</p>
         </div>
       ) : (
-        <Table>
-          <TableHeader className="bg-slate-200">
-            <TableRow>
-              <TableHead className="w-[150px] font-bold">Point</TableHead>
-              <TableHead className="font-bold">Tags</TableHead>
-              <TableHead className="text-right font-bold">Coords</TableHead>
-              <TableHead className="text-right font-bold w-24">
-                Actions
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {markers.map((marker) => (
-              <TableRow key={marker.id}>
-                <TableCell className="font-medium whitespace-nowrap truncate">
-                  {marker.name}
-                </TableCell>
-                <TableCell>
-                  <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                    {marker.tags}
-                  </span>
-                </TableCell>
-                <TableCell className="text-center w-24">
-                  <Button
-                    className="p-1 w-6 h-6 rounded-full"
-                    onClick={() => copyToClipboard(marker)}
-                  >
-                    {!showPopupCopy[marker.id] ? (
-                      <ClipboardDocumentIcon />
-                    ) : (
-                      <CheckIcon />
-                    )}
-                  </Button>
-                </TableCell>
-                <TableCell className="text-center">
-                  <Button className="p-1 w-6 h-6 rounded-full">
-                    <MapPinIcon />
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        <>
+          {isMobile ? (
+            <div className="flex flex-col space-y-4 p-4 h-full overflow-y-auto">
+              {markers.map((marker) => (
+                <div
+                  key={marker.id}
+                  className="p-4 bg-slate-200 rounded-lg shadow"
+                >
+                  <div className="font-bold">{marker.name}</div>
+                  <div className="flex mt-2 gap-4">
+                    <div className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full w-fit mr-auto">
+                      {marker.tags || "Autre"}
+                    </div>
+                    <Button
+                      className="p-1 w-6 h-6 rounded-full"
+                      onClick={() => copyToClipboard(marker)}
+                    >
+                      {!showPopupCopy[marker.id] ? (
+                        <ClipboardDocumentIcon />
+                      ) : (
+                        <CheckIcon />
+                      )}
+                    </Button>
+                    <Button className="p-1 w-6 h-6 rounded-full">
+                      <MapPinIcon />
+                    </Button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <Table>
+              <TableHeader className="bg-slate-200">
+                <TableRow>
+                  <TableHead className="w-[150px] font-bold">Point</TableHead>
+                  <TableHead className="font-bold">Tags</TableHead>
+                  <TableHead className="text-right font-bold">Coords</TableHead>
+                  <TableHead className="text-right font-bold w-24">
+                    Actions
+                  </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {markers.map((marker) => (
+                  <TableRow key={marker.id}>
+                    <TableCell className="font-medium whitespace-nowrap truncate">
+                      {marker.name}
+                    </TableCell>
+                    <TableCell>
+                      <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">
+                        {marker.tags || "Autre"}
+                      </span>
+                    </TableCell>
+                    <TableCell className="text-center w-24">
+                      <Button
+                        className="p-1 w-6 h-6 rounded-full"
+                        onClick={() => copyToClipboard(marker)}
+                      >
+                        {!showPopupCopy[marker.id] ? (
+                          <ClipboardDocumentIcon />
+                        ) : (
+                          <CheckIcon />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-center">
+                      <Button className="p-1 w-6 h-6 rounded-full">
+                        <MapPinIcon />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
+        </>
       )}
     </>
   );

b0f2d6404d0b688ca00ae5bfa738c91ac3779c8b
Kévin <kevin.blaise24@gmail.com>
2024-09-18 15:12:16 +0200

    feat(marker): add open in maps feature

--- src/components/marker/markersList.tsx
+++ src/components/marker/markersList.tsx
@@ -17,7 +17,7 @@ import {
     AlertDialogTrigger
 } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
-import { Check, Clipboard, MapPin, Trash } from 'lucide-react';
+import { Check, Clipboard, MapPin, Trash, SquareArrowOutUpRight } from 'lucide-react';
 
 interface MarkerListProps {
     markers: Marker[];
@@ -48,6 +48,10 @@ export const MarkersList = ({
 
     const goTo = (marker: Marker) => {
         router.push(`/map?lat=${marker.latitude}&lng=${marker.longitude}`);
+    }
+
+    const openInMaps = (marker: Marker) => {
+        window.open(`https://www.google.com/maps/search/?api=1&query=${marker.latitude},${marker.longitude}`);
     }
 
     const DeleteMarkerModal = ({ marker }: { marker: Marker }) => {
@@ -111,14 +115,17 @@ export const MarkersList = ({
                         <div className="bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full w-fit mr-auto">
                             {marker.tags || 'Autre'}
                         </div>
+                        <Button className="p-1 w-6 h-6 rounded-full bg-green-100 hover:bg-green-200 text-green-700 transition-colors duration-300">
+                            <MapPin onClick={() => goTo(marker)} className="w-4 h-4" />
+                        </Button>
                         <Button
                             className="p-1 w-6 h-6 rounded-full bg-blue-100 hover:bg-blue-200 text-blue-700 transition-colors duration-300"
                             onClick={() => copyToClipboard(marker)}
                         >
                             {!showPopupCopy[marker.id] ? <Clipboard className="w-4 h-4" /> : <Check className="w-4 h-4" />}
                         </Button>
-                        <Button className="p-1 w-6 h-6 rounded-full bg-green-100 hover:bg-green-200 text-green-700 transition-colors duration-300">
-                            <MapPin onClick={() => goTo(marker)} className="w-4 h-4" />
+                        <Button className="p-1 w-6 h-6 rounded-full bg-blue-100 hover:bg-blue-200 text-blue-700 transition-colors duration-300">
+                            <SquareArrowOutUpRight onClick={() => openInMaps(marker)} className="w-4 h-4" />
                         </Button>
                         <DeleteMarkerModal marker={marker} />
                     </div>
@@ -146,14 +153,20 @@ export const MarkersList = ({
                         {marker.address || `${marker.latitude}, ${marker.longitude}`}
                     </p>
                     <div className="flex justify-end space-x-3">
+                        <Button className="p-2 w-10 h-10 rounded-full bg-green-100 hover:bg-green-200 text-green-700 transition-colors duration-300">
+                            <MapPin onClick={() => goTo(marker)} className="w-5 h-5" />
+                        </Button>
                         <Button
-                            className="p-2 w-10 h-10 rounded-full bg-green-100 hover:bg-green-200 text-green-700 transition-colors duration-300"
+                            className="p-2 w-10 h-10 rounded-full bg-blue-100 hover:bg-blue-200 text-blue-700 transition-colors duration-300"
                             onClick={() => copyToClipboard(marker)}
                         >
                             {!showPopupCopy[marker.id] ? <Clipboard className="w-5 h-5" /> : <Check className="w-5 h-5" />}
                         </Button>
-                        <Button className="p-2 w-10 h-10 rounded-full bg-blue-100 hover:bg-blue-200 text-blue-700 transition-colors duration-300">
-                            <MapPin onClick={() => goTo(marker)} className="w-5 h-5" />
+                        <Button
+                            className="p-2 w-10 h-10 rounded-full bg-blue-100 hover:bg-blue-200 text-blue-700 transition-colors duration-300"
+                            onClick={() => openInMaps(marker)}
+                        >
+                            <SquareArrowOutUpRight className="w-5 h-5" />
                         </Button>
                     </div>
                 </div>
